1b000d2cb6c88761c7e262e8bc0dbcc60e82122e
Robert Arredondo <robert14ci@gmail.com>
2024-05-22 22:50:54 -0500

    Actualizacion en la seccion de Acerca de(22-05-2024): Se agrego lista de redes sociales

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-//Swiper
+//header scroll
+let header = document.querySelector('header');
 
+window.addEventListener('scroll',() => { 
+    header.classList.toggle('shadow', window.scrollY > 0);   
+});
+
+
+
+//Swiper
 var swiper = new Swiper(".home", {
         spaceBetween: 30,
         centeredSlides: true,
